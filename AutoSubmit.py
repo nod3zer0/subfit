3c546a9396a373bd528594a72188c2e0037e73d4
nod3zer0 <xceska06@stud.fit.vutbr.cz>
2023-11-30 03:59:26 +0100

    comparing hashes

--- AutoSubmit.py
+++ AutoSubmit.py
@@ -5,6 +5,7 @@ import shutil
 import os
 import sys
 import time
+import config
 
 
 def login():
@@ -22,8 +23,8 @@ def login():
     'login_form' : 1,
     'sentTime' : round(time.time()),
     'sv[fdkey]' : fdkey,
-    'LDAPlogin' : '',
-    'LDAPpasswd' : '',
+    'LDAPlogin' : config.username,
+    'LDAPpasswd' : config.password,
     'login' : ''
 
     }
@@ -45,13 +46,21 @@ def upload_file_session(s, url, file_path):
     if (text1):
         deleteUrl = "https://www.vut.cz/studis/" +text1[0]['href']
         r = s.get(deleteUrl, stream=True)
-        print(r.status_code)
+        if (r.status_code == 200):
+             print("OK")
+        else:
+            print("Error")
         print("deleting file on URL: " +deleteUrl)
 
     # get keys
     print("getting keys")
     r = s.get(url, stream=True)
-    print(r.status_code)
+
+    if (r.status_code == 200):
+         print("OK")
+    else:
+        print("Error")
+
     soup = bs.BeautifulSoup(r.text,'lxml')
     supa = soup.find('input',attrs={'id' : 's_tkey'})
     s_tkey = supa.get('value')
@@ -76,55 +85,37 @@ def upload_file_session(s, url, file_path):
     # upload file
     print("uploading file...")
     response = s.post(url,data=Data, files=files)
-    print(response.status_code)
+    if (r.status_code == 200):
+         print("OK")
+    else:
+        print("Error")
     print("file uploaded")
 
-def upload_file_cookie(c, url, file_path):
-    r = requests.get(url,cookies=c, stream=True)
-    # get delete file url
-    r = requests.get(url,cookies=c, stream=True)
+def downloadFile(s, url, filename):
+    r = s.get(url, stream=True)
     soup = bs.BeautifulSoup(r.text,'lxml')
-    text1 = soup.find_all('a', string = "Smazat")
 
-    # delete file if exists
-    if (text1):
-        deleteUrl = "https://www.vut.cz/studis/" +text1[0]['href']
-        r = requests.get(deleteUrl,cookies=c, stream=True)
-        print(r.status_code)
-        print("deleting file on URL: " +deleteUrl)
+    supa =  soup.find_all('a', string = filename)
+    downloadUrl = "https://www.vut.cz/studis" + supa[0]['href'].removeprefix(".")
+    print("downloading file:" + filename + " from URL: " + downloadUrl)
+    print(downloadUrl)
+    r = s.get(downloadUrl, stream=True)
+    if (r.status_code == 200):
+         print("OK")
+    else:
+        print("Error")
+    with open("downloaded", 'wb') as f:
+        r.raw.decode_content = True
+        shutil.copyfileobj(r.raw, f)
+    print("file downloaded")
 
-    # get keys
-    print("getting keys")
-    r = requests.get(url,cookies=c, stream=True)
-    print(r.status_code)
-    soup = bs.BeautifulSoup(r.text,'lxml')
-    supa = soup.find('input',attrs={'id' : 's_tkey'})
-    s_tkey = supa.get('value')
-    supa = soup.find('input',attrs={'id' : 's_key'})
-    s_key = supa.get('value')
-
-
-    # file to upload
-    files = {
-        'soubor': open(file_path, 'rb')
-    }
-
-    # data to upload
-    Data = {
-        's_tkey' : s_tkey,
-        's_key' : s_key,
-        'formID' : 'formAddFile',
-        'btnSubmit' : 1,
-        'soubor' : open(file_path, 'rb')
-    }
-
-    # upload file
-    print("uploading file...")
-    response = requests.post(url,data=Data,cookies=c, files=files)
-    print(response.status_code)
-    print("file uploaded")
-
-
+def compareHashes(file1, file2):
+    hash1 = os.system("md5sum " + file1)
+    hash2 = os.system("md5sum " + file2)
+    if (hash1 == hash2):
+        return True
+    else:
+        return False
 
 cj = browser_cookie3.brave()
 
@@ -133,7 +124,22 @@ url = sys.argv[1]
 #'xceska06.zip'
 file_path = sys.argv[2]
 
-s = login()
 
-#upload_file_session(s, url, file_path)
-upload_file_cookie(cj, url, file_path)
+s = requests.Session()
+s.cookies = cj
+
+upload_file_session(s, url, file_path)
+
+
+print("checking if file was uploaded correctly.")
+downloadFile(s, url, os.path.basename(file_path))
+
+print("comparing hashes")
+if (compareHashes(file_path, "downloaded")):
+    print("upload succesfull: files are equal")
+else:
+    print("upload unsuccesfull: files are not equal")
+
+
+
+#upload_file_cookie(cj, url, file_path)
